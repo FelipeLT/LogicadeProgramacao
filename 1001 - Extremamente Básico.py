30f2401f29de1f8b378721803c62add5930db143
FelipeLT <71089456+FelipeLT@users.noreply.github.com>
2020-12-22 12:38:57 -0300

    Create 1001 - Extremamente Básico.py

---
+++ 1001 - Extremamente Básico.py
@@ -0,0 +1,20 @@
+// Exercicio: URI 1001 - Extremamente Básico
+https://www.urionlinejudge.com.br/judge/pt/problems/view/1001 // 
+
+A = None
+B = None
+X = None
+
+def read_integer():
+  try:
+    # read for Python 2.x
+    return int(raw_input())
+  except NameError:
+    # read for Python 3.x
+    return int(input())
+
+
+A = read_integer()
+B = read_integer()
+X = A + B
+print(str("X = ") + str(X))
